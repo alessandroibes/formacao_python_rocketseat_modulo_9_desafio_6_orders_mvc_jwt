25d76ed765f20846ffd53e5631f7c25941c9d1df
alessandroibes <alessandro.ibes@gmail.com>
2025-05-21 18:46:39 -0300

    Refactoring login creator unit test

--- src/controllers/login_creator_test.py
+++ src/controllers/login_creator_test.py
@@ -1,20 +1,30 @@
+import pytest
+
+from src.drivers.password_handler import PasswordHandler
 from .login_creator import LoginCreator
 
 
+username = "teste"
+password = "tesTando"
+hashed_password = PasswordHandler().encrypt_password(password)
+
+
 class MockUserRepository:
-    def get_user_by_username(self, username: str) -> tuple[int, int, str]:
-        return (1, username, b"$2b$12$0iAKprs.kCuRQ/ldb4S2bOIh3aEhjYIbxwkO5TN2SmFLjoRDsDEZu")
+    def get_user_by_username(self, username):
+        return (10, username, hashed_password)
 
 
 def test_create():
-    repository = MockUserRepository()
-    controller = LoginCreator(repository)
-
-    username = "teste"
-    password = "tesTando"
-
-    response = controller.create(username, password)
+    login_creator = LoginCreator(MockUserRepository())
+    response = login_creator.create(username, password)
 
     assert response["access"] is True
     assert response["username"] == username
     assert response["token"] is not None
+
+
+def test_create_with_wrong_password():
+    login_creator = LoginCreator(MockUserRepository())
+
+    with pytest.raises(Exception):
+        login_creator.create(username, "senhaErrada")
